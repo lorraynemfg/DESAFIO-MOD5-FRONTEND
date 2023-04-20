1fb81ec8bdf812b91697a12cba75ed23fc6ab8a2
Guilherme Neves <nevessilvagui@gmail.com>
2023-04-19 21:01:42 -0300

    Branch Guilherme finalizada

--- primeirosprint/src/Components/MenuLateral/index.jsx
+++ primeirosprint/src/Components/MenuLateral/index.jsx
@@ -1,5 +1,7 @@
 import iconeHome from "../../assets/icone-home.svg";
+import iconeHomeSelecionado from "../../assets/icone-home-selecionado.svg";
 import iconeClientes from "../../assets/icone-clientes.svg";
+import iconeClientesSelecionado from "../../assets/icone-clientes-selecionado.svg";
 import iconeCobrancas from "../../assets/icone-cobranca.svg";
 import { useNavigate } from "react-router-dom";
 import "./style.css";
@@ -10,14 +12,56 @@ function MenuLateral() {
   return (
     <div className="section">
       <div className="container-menu">
-        <div className="icones" onClick={() => navigate("/resumo")}>
-          <img src={iconeHome} alt="icone-home" />
-          <span>Home</span>
+        <div
+          className={
+            window.location.pathname === "/resumo"
+              ? "icones selecionado"
+              : "icones"
+          }
+          onClick={() => navigate("/resumo")}
+        >
+          <img
+            src={
+              window.location.pathname === "/resumo"
+                ? iconeHomeSelecionado
+                : iconeHome
+            }
+            alt="icone-home"
+          />
+          <span
+            className={
+              window.location.pathname === "/resumo" ? "texto-selecionado" : ""
+            }
+          >
+            Home
+          </span>
         </div>
 
-        <div className="icones" onClick={() => navigate("/clientes")}>
-          <img src={iconeClientes} alt="icone-clientes" />
-          <span>Clientes</span>
+        <div
+          className={
+            window.location.pathname === "/clientes"
+              ? "icones selecionado"
+              : "icones"
+          }
+          onClick={() => navigate("/clientes")}
+        >
+          <img
+            src={
+              window.location.pathname === "/clientes"
+                ? iconeClientesSelecionado
+                : iconeClientes
+            }
+            alt="icone-clientes"
+          />
+          <span
+            className={
+              window.location.pathname === "/clientes"
+                ? "texto-selecionado"
+                : ""
+            }
+          >
+            Clientes
+          </span>
         </div>
 
         <div className="icones">
